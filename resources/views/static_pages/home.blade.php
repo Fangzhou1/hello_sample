85e091f91476a1d1cd402f11df7ae8e29460bad9
Fangzhou <13971192686@139.com>
2018-02-07 08:27:26 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,16 @@
 @section('title','主页')
 
 @section('content')
-  <h1>主页</h1>
+<div class="jumbotron">
+  <h1>Hello，朋友 </h1>
+  <p class="lead">
+    武汉移动工程建设部工程审计工作
+  </p>
+  <p>
+    一切，将从这里开始。
+  </p>
+  <p>
+    <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+  </p>
+</div>
 @stop
