722809269f7aa6937fbd92c11e4b45c70acb8533
Fangzhou1 <13971192686@139.com>
2018-03-09 20:27:50 +0000

    new4

--- database/migrations/2018_03_07_063147_create_settlementtimes_table.php
+++ database/migrations/2018_03_07_063147_create_settlementtimes_table.php
@@ -14,7 +14,7 @@ class CreateSettlementtimesTable extends Migration
     public function up()
     {
         Schema::create('settlementtimes', function (Blueprint $table) {
-            $table->increments('id')->primary();
+            $table->increments('id');
             $table->string('finished_ordernum');
             $table->string('finished_projectnum');
             $table->timestamps();
